9a1141d16ab95b5c312475f9f1eedb379e311012
angelllacuna <201880565@psu.palawan.edu.ph>
2021-07-18 19:54:53 +0800

    added

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 <footer class="mastfoot mt-auto" style="text-align: center;">
     <div class="inner">
-      <p>Created by <a href="https://github.com/emshii">emshii</a> for BSIT3-B1-A</p>
+      <p>Created by <a href="https://github.com/angelllacuna">angelllacuna</a> from BSIT3-B1-A</p>
     </div>
 </footer>
   </body>
